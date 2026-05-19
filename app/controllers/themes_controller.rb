41ec6772a5ea0bcd47e081684307ce9e4fe30f15
Luka <271838895+8luka@users.noreply.github.com>
2026-05-19 11:44:46 +0900

    Built the themes/new page and the corresponding methods in themes controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -6,8 +6,22 @@ class ThemesController < ApplicationController
   end
 
   def new
+    @theme = Theme.new
   end
 
   def create
+    @theme = Theme.new(theme_params)
+    @theme.user = current_user
+    if @theme.save
+      redirect_to theme_path(@theme)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:name, :specs)
   end
 end
