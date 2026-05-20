734b0cddc123ae9e418cd336c73a045571df3db2
Luka <271838895+8luka@users.noreply.github.com>
2026-05-20 12:53:12 +0900

    if condition for HTML safe

--- app/javascript/controllers/collapse_controller.js
+++ app/javascript/controllers/collapse_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="collapse"
 export default class extends Controller {
+  static targets = ["collapse", "label", "chevron"]
+
   connect() {
+    this.collapseTarget.addEventListener("show.bs.collapse", this.onShow)
+    this.collapseTarget.addEventListener("hide.bs.collapse", this.onHide)
+  }
+
+  disconnect() {
+    this.collapseTarget.removeEventListener("show.bs.collapse", this.onShow)
+    this.collapseTarget.removeEventListener("hide.bs.collapse", this.onHide)
+  }
+
+  onShow = () => {
+    this.labelTarget.textContent = "Click to hide"
+    this.chevronTarget.style.transform = "rotate(180deg)"
+  }
+
+  onHide = () => {
+    this.labelTarget.textContent = "Click to create an element"
+    this.chevronTarget.style.transform = "rotate(0deg)"
   }
 }
