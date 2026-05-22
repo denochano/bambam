94f1a65f73ebd33e47c9c34af10920db91ac3532
João Santos <joao.saul.santos@gmail.com>
2026-05-22 08:45:45 +0100

    bg jobs

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module UiKitt
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :solid_queue
+    config.mission_control.jobs.http_basic_auth_enabled = false
   end
 end
---
+++ test/jobs/element_build_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ElementBuildJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
